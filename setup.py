42aea68373109141745217227ce3e1c0fc34d4a6
Carlo Fisicaro <carlo@peptone.io>
2021-11-10 16:44:51 +0100

    Added adopt package

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+import setuptools
+
+with open("adopt/version.py") as infile:
+    exec(infile.read())
+
+with open("README.md") as f:
+    readme = f.read()
+
+setup(
+    name='adopt',
+    version=version,
+    packages=setuptools.find_packages(),
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url='',
+    license='',
+    author='Peptone Ltd.',
+    author_email='kamil@peptone.io',
+    description=' Attention DisOrder PredicTor (adopt): intrinsic protein disorder prediction throughdeep bidirectional transformers',
+    zip_safe=True,
+)
