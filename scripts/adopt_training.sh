05b574c4589223bbba2e18169b1d0bc7581aae8e
Carlo Fisicaro <carlo@peptone.io>
2021-11-12 22:27:04 +0100

    style(licence): apply MIT licence

--- scripts/adopt_training.sh
+++ scripts/adopt_training.sh
@@ -1,3 +1,8 @@
+# Copyright (c) 2021 Peptone.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
 #!/bin/bash   
 
 TRAIN_FASTA_FILE_PATH="<train_chezod_fasta_file_path>"
