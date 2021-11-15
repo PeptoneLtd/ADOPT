f8055a840f23de041e99f2f59759fda02e8eb3cb
Carlo Fisicaro <carlo@peptone.io>
2021-11-15 02:09:36 +0100

    feat: customise linter

--- adopt/__init__.py
+++ adopt/__init__.py
@@ -3,6 +3,8 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+"@generated"
+
 from . import constants, utils
 from .data import CheZod
 from .training import DisorderPred
