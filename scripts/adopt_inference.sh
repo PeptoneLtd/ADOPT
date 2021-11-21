4b46e8a690002d2c23a4b93b57a08125bcf44bbb
Carlo Fisicaro <66260709+CFisicaro@users.noreply.github.com>
2021-11-21 13:23:40 +0100

    perf: model for inference phase

--- scripts/adopt_inference.sh
+++ scripts/adopt_inference.sh
@@ -8,7 +8,7 @@
 NEW_PROT_FASTA_FILE_PATH="<new_proteins_fasta_file_path>" # put here your path
 NEW_PROT_RES_REPR_DIR_PATH="<new_proteins_residue_level_representation_dir>" # put here your path
 TRAIN_STRATEGY="train_on_cleared_1325_test_on_117_residue_split"
-MODEL_TYPE="combined"
+MODEL_TYPE="esm-1b"
 PRED_Z_FILE_PATH="../predicted_z_scores.json"
 
 printf "Extracting residue level representation of %s \n" $NEW_PROT_FASTA_FILE_PATH
