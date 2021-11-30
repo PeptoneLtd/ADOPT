bc152c9adbeabb94fe8cf7914d3509d7beeb3c0d
cellini-tone <istvan@peptone.io>
2021-11-30 22:42:22 +0000

    fix: lint clean up

--- scripts/extract_esm_msa_repr.py
+++ scripts/extract_esm_msa_repr.py
@@ -13,31 +13,29 @@ from adopt import utils
 my_parser = argparse.ArgumentParser(description='Get the path to .a3m files for the sequences in the fasta file')
 
 my_parser.add_argument('fasta_file',
-                        action='store',
-                        type=str
-                        )
+                       action='store',
+                       type=str)
 my_parser.add_argument('repr_path',
-                        action='store',
-                        type=str,
-                        help='path to save the esm-msa representations'
-                        )
+                       action='store',
+                       type=str,
+                       help='path to save the esm-msa representations')
 
 # Execute the parse_args() method
 args = my_parser.parse_args()
 
-a3m_input_path = '/msas/' #args.msa_path
+a3m_input_path = '/msas/'  # args.msa_path
 ff_path = args.fasta_file
 repr_path = args.repr_path
-DEFAULT_MSA_DEPTH = constants.msa_depth #args.msa_depth
+DEFAULT_MSA_DEPTH = constants.msa_depth  # args.msa_depth
 
-# get the identifiers from the fasta file
+# Get the identifiers from the fasta file
 df_msas = utils.fasta_to_df(ff_path)
 
 # Check if the msas are available for the sequences provided in the fasta file
 # ----------------------------------------------------------------------------
 msa_store = list(next(os.walk(a3m_input_path))[2])
 missing_msas = [seq for seq in list(df_msas['brmid']) if seq not in msa_store]
-if len(missing_msas)!=0:
+if len(missing_msas) != 0:
     print("No msa files were found for the following sequences: ", missing_msas)
     print("Generate msa files first!")
 
@@ -47,7 +45,7 @@ msa_transformer, msa_alphabet = esm.pretrained.esm_msa1b_t12_100M_UR50S()
 msa_transformer = msa_transformer.eval()
 msa_batch_converter = msa_alphabet.get_batch_converter()
 
-### Utilities for esm model
+# Utilities for esm model
 # -------------------------
 # This is an efficient way to delete lowercase characters and insertion characters from a string
 deletekeys = dict.fromkeys(string.ascii_lowercase)
@@ -60,9 +58,11 @@ def read_sequence(filename: str):
     record = next(SeqIO.parse(filename, "fasta"))
     return record.description, str(record.seq)
 
+
 def remove_insertions(sequence: str):
     """ Removes any insertions into the sequence. Needed to load aligned sequences in an MSA. """
     return sequence.translate(translation)
+
 
 def read_msa(filename: str, nseq: int):
     """ Reads the first nseq sequences from an MSA file, automatically removes insertions."""
@@ -78,15 +78,15 @@ msas_data = list(map(read_msa, [a3m_input_path+f_name for f_name in list(df_msas
 for protein_id in msas_data:
     msa_data = [protein_id]
     msa_batch_labels, msa_batch_strs, msa_batch_tokens = msa_batch_converter(msa_data)
-    #msa_batch_tokens = msa_batch_tokens.cuda()
+    # msa_batch_tokens = msa_batch_tokens.cuda()
 
     with torch.no_grad():
         results = msa_transformer(msa_batch_tokens, repr_layers=[12], return_contacts=True)
 
     # saving
     sv = OrderedDict()
-    sv['logits'] = results['logits'][0][0][1 : , ...].clone()
-    sv['representations'] = results['representations'][12][0][0][1 : , ...].clone()
+    sv['logits'] = results['logits'][0][0][1: , ...].clone()
+    sv['representations'] = results['representations'][12][0][0][1: , ...].clone()
     sv['contacts'] = results['contacts'][0].clone()
 
     print(f'Saving - {msa_batch_labels[0][0]}.pt')
