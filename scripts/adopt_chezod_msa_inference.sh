8bf6de42c5d7f1b34ea51cb7635fdffffe9e5652
Carlo Fisicaro <carlo@peptone.io>
2021-12-05 15:11:05 +0100

    fix: script paths

--- scripts/adopt_chezod_msa_inference.sh
+++ scripts/adopt_chezod_msa_inference.sh
@@ -5,12 +5,13 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+# The work directory is </ADOPT> in the <adopt> container
 LOCAL_MSA_DIR="../msa"
 NEW_PROT_FASTA_FILE_PATH="<new_proteins_fasta_file_path>" # put here your path
 NEW_PROT_RES_REPR_DIR_PATH="<new_proteins_residue_level_representation_dir>" # put here your path
 TRAIN_STRATEGY="train_on_cleared_1325_test_on_117_residue_split"
 MODEL_TYPE="esm-msa"
-PRED_Z_FILE_PATH="../predicted_z_scores.json"
+PRED_Z_FILE_PATH="predicted_z_scores.json"
 
 printf "Setting up the MSA procedure \n"
 bash adopt_msa_setup.sh $LOCAL_MSA_DIR
@@ -19,17 +20,17 @@ printf "Extracting Multi Sequence Alignments of %s \n" $NEW_PROT_FASTA_FILE_PATH
 bash msa_generator.sh $NEW_PROT_FASTA_FILE_PATH
 
 printf "Extracting residue level representation of %s \n" $NEW_PROT_FASTA_FILE_PATH
-docker exec -it adopt python ../adopt/embedding.py -f $NEW_PROT_FASTA_FILE_PATH \
-                                                   -r $NEW_PROT_RES_REPR_DIR_PATH \
-                                                   -a
+docker exec -it adopt python adopt/embedding.py -f $NEW_PROT_FASTA_FILE_PATH \
+                                                -r $NEW_PROT_RES_REPR_DIR_PATH \
+                                                -a
 
 printf "Computing Z scores of %s \n" $NEW_PROT_FASTA_FILE_PATH
-docker exec -it adopt python ../adopt/inference.py -s $TRAIN_STRATEGY \
-                                                   -m $MODEL_TYPE \
-                                                   -f $NEW_PROT_FASTA_FILE_PATH \
-                                                   -r $NEW_PROT_RES_REPR_DIR_PATH \
-                                                   -p $PRED_Z_FILE_PATH \
-                                                   -a
+docker exec -it adopt python adopt/inference.py -s $TRAIN_STRATEGY \
+                                                -m $MODEL_TYPE \
+                                                -f $NEW_PROT_FASTA_FILE_PATH \
+                                                -r $NEW_PROT_RES_REPR_DIR_PATH \
+                                                -p $PRED_Z_FILE_PATH \
+                                                -a
 
 docker cp adopt:$NEW_PROT_FASTA_FILE_PATH $NEW_PROT_FASTA_FILE_PATH
 printf "The trained models have been stored in %s \" $NEW_PROT_FASTA_FILE_PATH
--- scripts/adopt_chezod_msa_training.sh
+++ scripts/adopt_chezod_msa_training.sh
@@ -5,14 +5,15 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+# The work directory is </ADOPT> in the <adopt> container
 LOCAL_MSA_DIR="../msa"
-TRAIN_FASTA_FILE_PATH="../datasets/chezod_1325_all.fasta"
-TEST_FASTA_FILE_PATH="../datasets/chezod_117_all.fasta"
-TRAIN_RES_REPR_DIR_PATH="../representations/chezod_1325"
-TEST_RES_REPR_DIR_PATH="../representations/chezod_117"
+TRAIN_FASTA_FILE_PATH="datasets/chezod_1325_all.fasta"
+TEST_FASTA_FILE_PATH="datasets/chezod_117_all.fasta"
+TRAIN_RES_REPR_DIR_PATH="representations/chezod_1325"
+TEST_RES_REPR_DIR_PATH="representations/chezod_117"
 TRAIN_STRATEGY="<training_strategy>" # choose a training strategy
-TRAIN_JSON_FILE_PATH="../datasets/1325_dataset_raw.json"
-TEST_JSON_FILE_PATH="../datasets/117_dataset_raw.json" 
+TRAIN_JSON_FILE_PATH="datasets/1325_dataset_raw.json"
+TEST_JSON_FILE_PATH="datasets/117_dataset_raw.json" 
 
 printf "Setting up the MSA procedure \n"
 bash adopt_msa_setup.sh $LOCAL_MSA_DIR
@@ -24,22 +25,22 @@ printf "Extracting Multi Sequence Alignments of %s \n" $TEST_FASTA_FILE_PATH
 bash msa_generator.sh $TEST_FASTA_FILE_PATH
 
 printf "Extracting residue level representations of %s \n" $TRAIN_FASTA_FILE_PATH
-docker exec -it adopt python ../adopt/embedding.py -f $TRAIN_FASTA_FILE_PATH \
-                                                   -r $TRAIN_RES_REPR_DIR_PATH \
-                                                   -a
+docker exec -it adopt python adopt/embedding.py -f $TRAIN_FASTA_FILE_PATH \
+                                                -r $TRAIN_RES_REPR_DIR_PATH \
+                                                -a
 
 printf "Extracting residue level representations of %s \n" $TEST_FASTA_FILE_PATH
-docker exec -it adopt python ../adopt/embedding.py -f $TEST_FASTA_FILE_PATH \
-                                                   -r $TEST_RES_REPR_DIR_PATH \
-                                                   -a
+docker exec -it adopt python adopt/embedding.py -f $TEST_FASTA_FILE_PATH \
+                                                -r $TEST_RES_REPR_DIR_PATH \
+                                                -a
 
 printf "Training ADOPT on %s \n" $TRAIN_FASTA_FILE_PATH
-docker exec -it adopt python ../adopt/training.py -s $TRAIN_STRATEGY \
-                                                  -t $TRAIN_JSON_FILE_PATH \
-                                                  -e $TEST_JSON_FILE_PATH \
-                                                  -r $TRAIN_RES_REPR_DIR_PATH \
-                                                  -p $TEST_RES_REPR_DIR_PATH \
-                                                  -a
+docker exec -it adopt python adopt/training.py -s $TRAIN_STRATEGY \
+                                               -t $TRAIN_JSON_FILE_PATH \
+                                               -e $TEST_JSON_FILE_PATH \
+                                               -r $TRAIN_RES_REPR_DIR_PATH \
+                                               -p $TEST_RES_REPR_DIR_PATH \
+                                               -a
 
 docker cp adopt:/ADOPT/models ../
 printf "The trained models have been stored in ../models"
