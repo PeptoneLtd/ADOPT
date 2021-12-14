1b77c55b470b493f50c95b5ef598975177972f53
Carlo Fisicaro <carlo@peptone.io>
2021-12-14 11:36:16 +0100

    fix: paths

--- scripts/adopt_chezod_benchmarks.sh
+++ scripts/adopt_chezod_benchmarks.sh
@@ -5,14 +5,14 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-TRAIN_FASTA_FILE_PATH="../datasets/chezod_1325_all.fasta"
-TEST_FASTA_FILE_PATH="../datasets/chezod_117_all.fasta"
-TRAIN_RES_REPR_DIR_PATH="../representations/chezod_1325"
-TEST_RES_REPR_DIR_PATH="../representations/chezod_117"
+TRAIN_FASTA_FILE_PATH="datasets/chezod_1325_all.fasta"
+TEST_FASTA_FILE_PATH="datasets/chezod_117_all.fasta"
+TRAIN_RES_REPR_DIR_PATH="representations/chezod_1325"
+TEST_RES_REPR_DIR_PATH="representations/chezod_117"
 BENCHMARK_DATA_PATH="<benchmark_prediction_path>" # path of the predictions from the method you want to benchmark
 TRAIN_STRATEGY="<training_strategy>" # choose a training strategy
-TRAIN_JSON_FILE_PATH="../datasets/1325_dataset_raw.json"
-TEST_JSON_FILE_PATH="../datasets/117_dataset_raw.json" 
+TRAIN_JSON_FILE_PATH="datasets/1325_dataset_raw.json"
+TEST_JSON_FILE_PATH="datasets/117_dataset_raw.json" 
 
 printf "Extracting residue level representation of %s \n" $TRAIN_FASTA_FILE_PATH
 
@@ -25,7 +25,7 @@ python adopt/embedding.py $TEST_FASTA_FILE_PATH \
                           $TEST_RES_REPR_DIR_PATH
 
 printf "Benchmarking predictions in %s \n" $BENCHMARK_DATA_PATH
-printf "The results are stored in ../media \n"
+printf "The results are stored in media \n"
 
 python adopt/benchmarks.py $BENCHMARK_DATA_PATH \
                            $TRAIN_JSON_FILE_PATH \
